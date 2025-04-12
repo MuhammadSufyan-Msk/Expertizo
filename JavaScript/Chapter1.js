b35e044596630a427e75746b1bbd175f170a761f
Muhammad Sufyan <msufiyan2003@gmail.com>
2025-04-12 15:32:16 +0500

    Changes

---
+++ JavaScript/Chapter1.js
@@ -0,0 +1,53 @@
+// // Method
+alert("Welcome to JavaScript");
+
+// Variable
+var firstName = prompt("Enter Your First Name:");
+var lastName = prompt("Enter Your Last Name:");
+
+alert(
+    "Welcome" + firstName + " " + lastName
+)
+
+// console.log(typeof userAge)
+// Concatination occur by adding integer & string together
+
+// var num1 = 10;
+// var num2 = 5;
+// var add = num1 + num2;
+// var sub = num1 - num2;
+// var mul = num1 * num2;
+// var div = num1 / num2;
+
+// console.log(add)
+// console.log(sub)
+// console.log(mul)
+// console.log(div)
+
+// var num = 10;
+// console.log(num++) // Post Increment = 10
+// console.log(num) // Incremented Result = 11
+
+// var num = 5;
+// console.log(++num) // Pre Incremented Result = 11
+
+//Post & Pre Incrementation
+// var a = 10;
+// var b = 15;
+// var c = a++ + ++b + a++ + b++ + ++a + b++ + a++ + ++b + a++ + b++;
+// console.log(c)
+
+// var num1 = 70;
+// var num2 = 80;
+// var num3 = num1++ + num2++ + ++num1 + num2++ + ++num1 + num2++ + num1++ + ++num2 + num1++ + num2++;
+// console.log(num3)
+
+// var firstName = "Muhammad";
+// var lastName = "Sufyan";
+// var fullName = firstName + " " + lastName;
+
+// alert(
+//     firstName + " " + lastName
+// )
+
+// console.log(fullName)
